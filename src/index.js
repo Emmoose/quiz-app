e786f271926340a3f18308554b9ec784cbf1d7a6
Emil Mossberg <emil.mossberg@gmail.com>
2018-09-04 23:59:44 +0200

    Testing GitHub

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
-//PRIO -
-//Get the american map again, removed agaid 
-
-
-//Styling work on header
-
-// Can I use less data?
-
-// push to github
-
-// Radio button selection must be better
-// Work over having to use the QuestioNSelector components inbetween
-// quizz app and Question, preferably not
-
-// Make it look decent on mobil
-
-// clean code, most of all styles.css
-
-// Work on Google Maps API feature, come up with how Im gonna use the map
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import QuizApp from './components/QuizApp'
